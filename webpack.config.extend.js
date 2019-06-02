e6d2228d043fadd90cc7d9c4fdfc6f637051732b
Simen <decisiveboy20@hotmail.com.rw>
2019-06-03 03:23:18 +0800

    added default false to jquery-datatable.net

--- webpack.config.extend.js
+++ webpack.config.extend.js
@@ -13,13 +13,13 @@
  *
  */
 
-const path = require('path');
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const rtlcssLoader = path.resolve(__dirname, 'rtlcss-loader.js');
+const path = require('path')
+const webpack = require('webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin
+const rtlcssLoader = path.resolve(__dirname, 'rtlcss-loader.js')
 
 module.exports = (webpackConfig, env, { paths }) => {
-
     // Support for RTL
     /*
     const isDev = webpackConfig.mode === 'development'
@@ -35,25 +35,31 @@ module.exports = (webpackConfig, env, { paths }) => {
     });
     */
 
+
     // Fix for flot resize
     webpackConfig.module.rules[2].oneOf.splice(0, 0, {
+        test: /datatables\.net.*.js$/,
+        use: ['imports-loader?define=>false'],
+    })
+
+    webpackConfig.module.rules[2].oneOf.splice(0, 0, {
         test: /jquery\.flot\.resize\.js$/,
-        use: ['imports-loader?this=>window']
-    });
+        use: ['imports-loader?this=>window'],
+    })
 
     // Support for use custom .eslintrc
     webpackConfig.module.rules.some(rule => {
         if (Array.isArray(rule.use)) {
             const eslintUse = rule.use.find(item =>
-                Object.keys(item.options).find(key => key === 'useEslintrc'),
-            );
-            eslintOptions = eslintUse && eslintUse.options;
+                Object.keys(item.options).find(key => key === 'useEslintrc')
+            )
+            eslintOptions = eslintUse && eslintUse.options
             if (eslintOptions) {
-                eslintOptions.useEslintrc = true;
-                return true;
+                eslintOptions.useEslintrc = true
+                return true
             }
         }
-    });
+    })
 
     // Set globals for external plugins
     webpackConfig.plugins = (webpackConfig.plugins || []).concat([
@@ -62,17 +68,17 @@ module.exports = (webpackConfig, env, { paths }) => {
             jQuery: 'jquery',
             'window.jQuery': 'jquery',
             'window.moment': 'moment',
-            'moment': 'moment',
-            Raphael: 'raphael' // required by morris.js
-        })
-    ]);
+            moment: 'moment',
+            Raphael: 'raphael', // required by morris.js
+        }),
+    ])
 
     // Allow cofiguration of router base href
     webpackConfig.plugins = (webpackConfig.plugins || []).concat([
         new webpack.DefinePlugin({
-            'PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL)
-        })
-    ]);
+            PUBLIC_URL: JSON.stringify(process.env.PUBLIC_URL),
+        }),
+    ])
 
     // Enable BundleAnalyzerPlugin
     // webpackConfig.plugins = (webpackConfig.plugins || []).concat([

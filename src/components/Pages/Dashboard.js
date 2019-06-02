1d2fd67dfe97a99851d38d8a2ca55a177e1d5c05
Simen <decisiveboy20@hotmail.com.rw>
2019-06-03 03:24:13 +0800

    added some text

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,62 +1,60 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react'
 
 // import { Link } from 'react-router-dom';
 
-import ContentWrapper from '../Layout/ContentWrapper';
-import { Row , Col } from 'reactstrap';
-import ItemCard from '../Products/ItemCard';
+import ContentWrapper from '../Layout/ContentWrapper'
+import { Row, Col } from 'reactstrap'
+import ItemCard from '../Products/ItemCard'
 
-import { request } from 'core/AjaxManager';
-import { route } from 'core/RouteManager';
+import { request } from 'core/AjaxManager'
+import { route } from 'core/RouteManager'
 
 class Dashboard extends Component {
-
     constructor(props) {
-        super();
+        super()
 
         this.state = {
             page: 1,
             items: [],
         }
     }
-    
+
     componentWillMount() {
-        const _ajax = request();
-        
-        _ajax.get(`${route('ajax.items')}?_today=1`).then((result) => {
+        const _ajax = request()
+
+        _ajax.get(`${route('ajax.items')}?_today=1`).then(result => {
             if (result.data) {
                 this.setState({
-                    items: result.data.items
-                });
+                    items: result.data.items,
+                })
             }
-        });
+        })
     }
-    
 
     render() {
-        return  (
+        return (
             <ContentWrapper>
                 <div className="content-heading">
                     <div>
-                        Console
-                        <small>Oub admin</small>
+                        儀錶板
+                        <small>任挑任選阿勇伯</small>
                     </div>
                 </div>
 
                 <h4 className="mt-0 page-header">今日精選</h4>
 
                 <Row>
-                    { this.state.items.map((item , index) => {
+                    {this.state.items.map((item, index) => {
                         return (
-                            <Col lg={ 6 } xl={ 4 } key={index}>
-                                <ItemCard item={item}/>
+                            <Col lg={6} xl={4} key={index}>
+                                <ItemCard item={item} />
                             </Col>
-                        );
-                    }) }
+                        )
+                    })}
                 </Row>
             </ContentWrapper>
         )
     }
 }
 
-export default Dashboard;
+export default Dashboard
